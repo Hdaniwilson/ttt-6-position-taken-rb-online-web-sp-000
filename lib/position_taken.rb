adf038c72479319cb267996b1f76830cfcb3754f
Heather Wilson <hdaniwilson@gmail.com>
2021-04-04 22:55:52 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,6 +3,8 @@ def position_taken?(board, index)
     return false
   elsif board[index] == ""
     return false
+  elsif board[index] == nil
+    return false
   elsif board[index] == "X" || board[index] == "O"
     return true
   end
